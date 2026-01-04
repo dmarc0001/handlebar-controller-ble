bb43b58df6dcc1ff5624d33fb66d0861c7bd8a08
Dirk Marciniak <dirk.marciniak@mail.de>
2026-01-04 18:56:15 +0100

    defaults corrected

--- include/main.hpp
+++ include/main.hpp
@@ -1,6 +1,14 @@
 #pragma once
+#include <esp_sleep.h>
+
+enum esp_sleep_type
+{
+  SLEEP_LIGHT,
+  SLEEP_DEEP
+};
+
+
 void setup();
 void loop();
-void make_sleep();
-
-
+void blink_blue_led( uint8_t, uint32_t _interval = 150 );
+esp_sleep_wakeup_cause_t make_sleep( esp_sleep_type );
